afb024978de1d1c87bc06f5f39b7535bf6413184
Mr-Fakename <dimitriravot@gmail.com>
2021-11-24 11:39:18 +0100

    Add user creation form + correct implementation of sticky nav bar
    Sticky nav bar was getting in the way of other elements; a clone has been implemented with jQuery and CSS transform to make room and follow scroll

--- purbeurre/urls.py
+++ purbeurre/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from users import views as users_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
+    path('register/', users_views.register, name='register'),
 ]
--- website/static/website/scripts.js
+++ website/static/website/scripts.js
@@ -48,3 +48,13 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+$(document).ready(function() {
+    let $nav = $("nav"),
+        $clone = $nav.before($nav.clone().addClass("clone"));
+
+    $(window).on("scroll", function() {
+        let fromTop = $(window).scrollTop();
+        $("body").toggleClass("down", (fromTop > 100));
+    });
+});
