39c918ace9b589bd07fee4b0920bd9ac862b90bf
Mr-Fakename <dimitriravot@gmail.com>
2021-12-22 22:51:50 +0100

    Update search and results display as class based view & uniformly scaled cards

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
+from .views import ProductsSearchListView
 from . import views
 
 app_name = 'website'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('products/', views.search, name='search'),
+    # path('products/', views.search, name='search'),
+    path('search', ProductsSearchListView.as_view(), name="search"),
 ]
--- website/views.py
+++ website/views.py
@@ -1,23 +1,27 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Product
+from django.views.generic import ListView
 
 
 def index(request):
     return render(request, 'website/index.html')
 
 
-def search(request):
-    query = request.GET.get('query')
-    if not query:
-        products = Product.objects.all()
-    else:
-        # title contains the query is and query is not sensitive to case.
+class ProductsSearchListView(ListView):
+    model = Product
+    template_name = 'website/products.html'
+    context_object_name = 'products'
+    paginate_by = 21
+
+    def get_queryset(self):
+        query = self.request.GET.get('query')
         products = Product.objects.filter(name__icontains=query)
-    if not products.exists():
-        products = Product.objects.filter(generic_name__icontains=query)
-    title = "Résultats pour la requête %s"%query
-    context = {
-        'products': products,
-        'title': title
-    }
-    return render(request, 'website/products.html', context)
+        if not products:
+            products = get_list_or_404(Product, generic_name__icontains=query)
+        return products
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductsSearchListView, self).get_context_data(*args, **kwargs)
+        query = self.request.GET.get('query')
+        context['title'] = "Résultats pour la requête %s" % query
+        return context
