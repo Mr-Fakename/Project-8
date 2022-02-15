926960f45e67db33ea84f96c380e3b9246b9ae8c
Mr-Fakename <58395707+Mr-Fakename@users.noreply.github.com>
2022-02-15 15:16:43 +0100

    Update settings.py

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x3et8s*gi=n$i#vmtsn#pv926y-2!$9nwqoa1tbl=mdn9c6wt&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -141,5 +140,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dimitriravot@gmail.com'
-EMAIL_HOST_PASSWORD = 'ssvweasbygvnngrj'
